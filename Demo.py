aeaf0b91e5ad0fff9ab00b807b7e5d9a94290dcf
rahulnkkk <147973211+rahulnkkk@users.noreply.github.com>
2025-03-18 09:35:54 +0530

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,15 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Welcome')
+Welcome
+>>> Print('Hoho')
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    Print('Hoho')
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+>>> 4+5
+9
+>>> 345+435*2*0
+345
+>>> print('Hello python')
+Hello python
